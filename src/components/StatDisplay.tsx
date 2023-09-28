95aca610c3140bb4c599fb3497c597ff940f2b74
Seth Church <sethcdev@gmail.com>
2023-09-27 22:59:35 -0400

    add stat display bonus value

--- src/components/StatDisplay.tsx
+++ src/components/StatDisplay.tsx
@@ -1,3 +1,5 @@
+import { Chip } from '@nextui-org/react';
+
 import { CharacterSheetPod } from './CharacterSheetPod';
 
 interface StatDisplayProps {
@@ -5,9 +7,12 @@ interface StatDisplayProps {
 }
 
 const StatDisplay = ({ stat }: StatDisplayProps) => {
+  const statBonus = Math.floor(stat / 2) - 5;
+
   return (
-    <CharacterSheetPod className="aspect-square" variant="alt">
+    <CharacterSheetPod className="flex aspect-square flex-col items-center justify-center px-6" variant="alt">
       <p className="m-auto text-3xl">{stat}</p>
+      <Chip className="px-4" variant="faded">{`${statBonus >= 0 ? '+' : ''}${statBonus}`}</Chip>
     </CharacterSheetPod>
   );
 };

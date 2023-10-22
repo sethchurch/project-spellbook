5c130799bdb27f1330ab54bc39ac9df69a3e74c8
sethchurch <sethcdev@gmail.com>
2023-10-22 11:24:31 -0400

    center

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,17 +1,17 @@
 import { Button } from '@nextui-org/button';
 import Link from 'next/link';
 
+import { Icon } from '@/components/Elements/Icon';
 import { Input } from '@/components/Elements/Input';
 import { ToggleThemeButton } from '@/components/Elements/ToggleThemeButton';
 
-import { Icon } from '../Elements/Icon';
 import { ProfileDropdown } from './ProfileDropdown';
 import { SideNavTrigger } from './SideNavTrigger';
 
 const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 col-span-2 m-0 flex h-16 w-full items-center justify-between gap-8 border border-none bg-stone-100 px-3 shadow-sm dark:bg-zinc-900 md:gap-32">
-      <div className="flex gap-3">
+      <div className="flex items-center gap-3">
         <SideNavTrigger>
           <Button isIconOnly>
             <Icon icon="menu" />
@@ -24,7 +24,7 @@ const Navbar = () => {
         </Link>
       </div>
       <Input isClearable isDisabled styleVariant="inset" variant="faded" />
-      <div className="flex gap-3">
+      <div className="flex items-center gap-3">
         <ToggleThemeButton />
         <ProfileDropdown />
       </div>
